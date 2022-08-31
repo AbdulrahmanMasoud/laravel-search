09207c65c36ec1ba6e78756acecefce7bb355678
AbdulrahmanMasoud <abdulrahman.masoud.official@gmail.com>
2022-08-31 02:07:09 +0200

    now can search in - one column - multiple columns - multiple columns with ther data - json column 👌

--- src/Searchable.php
+++ src/Searchable.php
@@ -6,17 +6,41 @@ use Illuminate\Database\Eloquent\Builder;
 
 trait Searchable
 {
-    public function scopeSearch(Builder $builder,string $searchIn,string $input)
+    public function scopeSearch(Builder $builder, string $searchIn, string $input)
     {
-        $builder->when(request()->has($input),function($q) use($searchIn,$input){    
-            $q->where($searchIn, 'LIKE' ,'%'.request($input).'%')->get();
+        $builder->when(request()->has($input), function ($q) use ($searchIn, $input) {
+            $q->where($searchIn, 'LIKE', '%'.request($input).'%');
         });
     }
-    public function scopeJsonSearch(Builder $builder,string $searchIn,$input)
-    {
-        $builder->when(request()->has($input),function($q) use($searchIn,$input){    
-            $q->whereJsonContains($searchIn, request($input))->get();
-        });
-    }  
 
+    public function scopeJsonSearch(Builder $builder, string $searchIn, string $input)
+    {
+        $builder->when(request()->has($input), function ($q) use ($searchIn, $input) {
+            // $q->whereJsonContains($searchIn, request($input));
+            $q->where($searchIn, "LIKE", '%'.request($input).'%');
+        });
+    }
+
+    public function scopeSearchMultiple(Builder $builder, $fields)
+    {
+        $builder->where(function ($q) use ($fields) {
+            foreach ($fields as $key => $field) {
+                if (!request()->has($field)) {
+                    continue;
+                }
+                $q->orWhere($key, "LIKE", "%".request($field)."%");
+            }
+        });
+    }
+
+    public function scopeSearchInMultiple(Builder $builder, array $searchIn, string $input)
+    {
+        $builder->when(request()->has($input), function ($q) use ($searchIn, $input) {
+            $q->where(function ($q) use ($searchIn, $input) {
+                foreach ($searchIn as $field) {
+                    $q->orWhere($field, 'LIKE', '%'.request($input).'%');
+                }
+            });
+        });
+    }
 }
